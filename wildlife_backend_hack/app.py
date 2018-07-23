42614d63508206895d183c430e5d619438317332
Niharika Dutta <nidutta@microsoft.com>
2018-07-23 16:31:20 -0700

    Restified with sample json

--- wildlife_backend_hack/app.py
+++ wildlife_backend_hack/app.py
@@ -3,17 +3,33 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
 
-@app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
+@app.route('/wir/api/get', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
 
 if __name__ == '__main__':
     import os
