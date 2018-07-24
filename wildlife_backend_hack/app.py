2dd2890f79502b48f3034ff4526f3a191b01b31a
niharikaduttams <nidutta@microsoft.com>
2018-07-24 23:26:01 +0000

    pushed frontend changes

--- wildlife_backend_hack/app.py
+++ wildlife_backend_hack/app.py
@@ -3,8 +3,19 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 app = Flask(__name__)
+#"C:\\repo\\wlir_web_ui\\wlir_web_ui\\dist"
+from pymongo import MongoClient
+import pprint
+
+
+def connect_database():
+    client = MongoClient()
+    db = client['wirDB']
+    table = db['userLabels']
+    result = table.find()
+    return result
 
 
 tasks = [
@@ -28,14 +39,32 @@ wsgi_app = app.wsgi_app
 
 @app.route('/wir/api/get', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    client = MongoClient()
+    db = client['wirDB']
+    table = db['userLabels2']
+    result = table.find()
+    return jsonify({'results': tasks})
 
+@app.route('/api')
+def return_api():
+    return jsonify({'result': tasks})
+
+
+
+@app.route('/')
+def welcome():
+    return render_template('index.html')
+
+    
 
 if __name__ == '__main__':
     import os
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('SERVER_PORT', '8080'))
     except ValueError:
-        PORT = 5555
+        PORT = 8080
+    connect_database()
+    print("blabla")
+    print("root folder web: ", app.instance_path)
     app.run(HOST, PORT)
