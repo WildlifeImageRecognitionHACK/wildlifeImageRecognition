edea28730f51a9bac74f205da868b95f50f48019
niharikaduttams <nidutta@microsoft.com>
2018-07-25 22:04:46 +0000

    added code to serve all images through an api

---
+++ wildlife_backend_hack/apis.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify, send_from_directory, url_for
+app = Flask(__name__, static_url_path='')
+from pymongo import MongoClient
+import json
+
+client = MongoClient()
+db = client['wirDB']
+images = {}
+
+
+def get_data_imagedb():
+    image_table = db['image']
+    image_data = image_table.find()
+    i = 0
+    for image in image_data:
+        images[i] = image
+        i += 1
+
+
+@app.route('/api')
+def return_image():
+    imageTable = db['organizations']
+    result = imageTable.find()
+    s = "{"
+    for res in result:
+        print(type(res))
+        s += str(res) + ","
+    s = s[:-1]
+    s = s.replace("'", '"')
+    s += "}"
+
+    return jsonify(s)
+
+
+@app.route('/')
+def welcome():
+    return app.send_static_file('index.html')
+
+
+@app.route('/images/')
+def get_all_images():
+    response = app.response_class(response=json.dumps(images),
+                                  status=200,
+                                  mimetype="application/json")
+    return response
+    # filename = "1039210COR1 031.jpg"
+    # MEDIA_FOLDER = "C:\\Images\\results\\Animals"
+    # imageLink = send_from_directory(MEDIA_FOLDER, filename, as_attachment=False)
+    # print(imageLink)
+    # return imageLink
+
+
--- wildlife_backend_hack/app.py
+++ wildlife_backend_hack/app.py
@@ -3,72 +3,21 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, send_from_directory, url_for
 app = Flask(__name__, static_url_path='')
-#"C:\\repo\\wlir_web_ui\\wlir_web_ui\\dist"
-from pymongo import MongoClient
-import json
-import pprint
+from apis import get_data_imagedb
 
-client = MongoClient()
-db = client['wirDB']
-
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
 
-@app.route('/wir/api/get', methods=['GET'])
-def get_tasks():
-    table = db['userLabels2']
-    result = db.list_collection_names()
-    
-    return jsonify({'results': tasks})
-
-@app.route('/api')
-def return_api():
-    imageTable = db['organizations']
-    result = imageTable.find()
-    s = "{"
-    for res in result:
-        s += str(res) + ","
-    s = s[:-1]
-    s = s.replace("'", '"')
-    s += "}"
-    print(s)
-    #json_res = json.loads(s)
-    #pprint(json_res)
-    #images = {}
-    #for i in range(result.count())
-     #   images[i] =
-    return jsonify(s)
-
-
-@app.route('/')
-def welcome():
-    return app.send_static_file('index.html')
-
-    
-
 if __name__ == '__main__':
     import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(os.environ.get('SERVER_PORT', '8080'))
     except ValueError:
         PORT = 8080
-    app.run('0.0.0.0', PORT)
+    get_data_imagedb()
+    app.run('0.0.0.0', PORT, True)
+    print("does the flow go here?")
